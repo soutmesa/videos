1c22e8da0420baa239e5e4fabe5f3300a241bc06
soutmesa <soutmesa3@gmail.com>
2016-10-31 10:52:58 +0700

    changed pc

---
+++ app/timedisplay.pipe.ts
@@ -0,0 +1,9 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'timeDisplay'})
+export class TimeDisplayPipe implements PipeTransform {
+    transform(value:number, args:string[]):string {
+        var hh = Math.floor(value / 3600), mm = Math.floor(value / 60) % 60, ss = Math.floor(value) % 60;
+        return hh + ":" + (mm < 10 ? "0" : "") + mm + ":" + (ss < 10 ? "0" : "") + ss;
+    }
+}
